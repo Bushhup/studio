965ebc784a0577ff1343e632eb30a5c3769affce
S MD Shabuddin Osaid <shabuddinaw@gmail.com>
2025-08-13 12:12:40 +0000

    Error: [next]/internal/font/google/inter_e5387405.module.css:62:9 [1m[

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['var(--font-inter)', 'sans-serif'],
-        headline: ['var(--font-space-grotesk)', 'sans-serif'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['"Space Grotesk"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
